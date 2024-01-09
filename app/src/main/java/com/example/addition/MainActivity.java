c44deae986d7ecf7ccd83138c29ae27febd5b2d1
sejal <sejalkulkarni2004@gmail.com>
2024-01-09 15:20:32 +0530

    Addition

--- app/src/main/java/com/example/addition/MainActivity.java
+++ app/src/main/java/com/example/addition/MainActivity.java
@@ -9,10 +9,10 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    EditText num1,num2;
-    TextView txt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +20,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         EditText num1=(EditText) findViewById(R.id.edittxt1);
         EditText num2=(EditText) findViewById(R.id.edittxt2);
-        Button btn=(Button) findViewById(R.id.btn1);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button button1=(Button) findViewById(R.id.btn1);
+        Button button2=(Button) findViewById(R.id.btn2);
+        Button button3=(Button) findViewById(R.id.btn3);
+        Button button4=(Button) findViewById(R.id.btn4);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              int x=Integer.parseInt(num1.getText().toString());
-              int y=Integer.parseInt(num2.getText().toString());
+                int x=Integer.parseInt(num1.getText().toString());
+                int y=Integer.parseInt(num2.getText().toString());
+
               Toast.makeText(getApplicationContext(),"Addition="+(x+y),Toast.LENGTH_LONG).show();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+                int x=Integer.parseInt(num1.getText().toString());
+                int y=Integer.parseInt(num2.getText().toString());
+                Toast.makeText(getApplicationContext(),"Substraction="+(x-y),Toast.LENGTH_LONG).show();
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+                int x=Integer.parseInt(num1.getText().toString());
+                int y=Integer.parseInt(num2.getText().toString());
+                Toast.makeText(getApplicationContext(),"Multiply="+(x*y),Toast.LENGTH_LONG).show();
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v) {
+                int x=Integer.parseInt(num1.getText().toString());
+                int y=Integer.parseInt(num2.getText().toString());
+                Toast.makeText(getApplicationContext(),"Division="+(x/y),Toast.LENGTH_LONG).show();
+
             }
         });
     }
